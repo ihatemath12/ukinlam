ffb1417857cb5209f841839470dba3f498ca453f
ihatemath12 <137325439+ihatemath12@users.noreply.github.com>
2023-06-22 16:12:02 +0800

    Add files via upload

---
+++ theme/js/type.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function() {
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+
+    });
+
+    // smooth scrolling 
+
+    $('a[href*="#"]').on('click', function(e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+                scrollTop: $($(this).attr('href')).offset().top,
+
+            },
+            500,
+            'linear'
+        );
+
+    });
+
+});
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+window.onscroll = function() { myFunction() };
+
+function myFunction() {
+
+    if (document.documentElement.scrollTop > 100) {
+        document.getElementById("about").className = "about animate__animated animate__backInUp ";
+    } else {}
+    if (document.documentElement.scrollTop > 1000) {
+        document.getElementById("my_study_competition").className = "my_study_competition animate__animated animate__backInUp ";
+    } else {
+
+    }
+
+    console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop > 1700) {
+        document.getElementById("certification").className = "certification animate__animated animate__backInUp ";
+    } else {}
+
+    if (document.documentElement.scrollTop > 2600) {
+        document.getElementById("contact").className = "contact animate__animated animate__backInUp ";
+        document.getElementById("add_br").className = "hide";
+    } else {}
+
+}
